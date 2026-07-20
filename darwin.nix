700c402a120f6111673c60c25de65169065a28e3
Zachery Faria <zacheryfaria@gmail.com>
2026-07-19 21:11:40 -0700

    fix nix in zshrc

--- darwin.nix
+++ darwin.nix
@@ -14,6 +14,15 @@
     pkgs.bash
   ];
 
+  programs.zsh = {
+    enable = true;
+    initExtraFirst = ''
+      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+      fi
+    '';
+  };
+
   home.sessionPath = [
     "/Users/${user}/.local/bin"
     "/Users/${user}/bin"
