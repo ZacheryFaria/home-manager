1d123a57f8545b85e429eeca89e8967494c8f093
Zachery Faria <zacheryfaria@gmail.com>
2026-08-18 12:13:04 -0700

    fix home macbook

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
         modules = [
-          ./home.nix
+          ./darwin.nix
           nixvim.homeModules.nixvim
         ];
 
